ea084a4375b6dc6b88259d5e18d7ae04ecc52576
rikardbq <j10rikardb@gmail.com>
2024-10-02 12:27:31 +0200

    init

---
+++ src/root/Root.tsx
@@ -0,0 +1,8 @@
+const Root = () => {
+  
+  return (
+    <div>Hello World</div>
+  );
+};
+
+export default Root;
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.tsx",
+  output: {
+    path: path.join(__dirname, "/dist"),
+    filename: "index.bundle.js",
+    clean: true
+  },
+  devServer: {
+    port: 3000,
+    compress: true,
+    open: true,
+    static: './dist',
+    client: {
+      logging: 'info',
+      overlay: true,
+    }
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  plugins: [new HtmlWebpackPlugin({ template: "./src/index.html" })],
+};
